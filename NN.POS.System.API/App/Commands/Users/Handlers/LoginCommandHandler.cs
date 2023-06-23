b021291ed242e438ce8c2bf87d734df5af55c0b2
codewithmecoder <yaty1031@gmail.com>
2023-06-23 07:05:22 +0700

    feat > login

---
+++ NN.POS.System.API/App/Commands/Users/Handlers/LoginCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using MediatR;
+using NN.POS.System.API.Core.Dtos.Users;
+using NN.POS.System.API.Core.IRepositories.Users;
+using NN.POS.System.API.Infra.Tables.User;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
+using NN.POS.System.API.Core.Entities.Users;
+using NN.POS.System.API.Core.Exceptions.Users;
+
+namespace NN.POS.System.API.App.Commands.Users.Handlers;
+
+public class LoginCommandHandler : IRequestHandler<LoginCommand, UserDto>
+{
+    private readonly IUserRepository _userRepository;
+    private readonly ITokenProvider _tokenProvider;
+    private readonly IPasswordHasher<UserEntity> _passwordHasher;
+
+    public LoginCommandHandler(IUserRepository userRepository, ITokenProvider tokenProvider, IPasswordHasher<UserEntity> passwordHasher)
+    {
+        _userRepository = userRepository;
+        _tokenProvider = tokenProvider;
+        _passwordHasher = passwordHasher;
+    }
+
+    public async Task<UserDto> Handle(LoginCommand request, CancellationToken cancellationToken)
+    {
+        var user = await _userRepository.GetByUserNameAsync(request.Username) ?? throw new InvalidCredentialException();
+        if (!user.ValidatePassword(request.Password, _passwordHasher)) throw new InvalidCredentialException();
+
+        var token = _tokenProvider.CreateToken(new Claim[]
+        {
+            new ("userId", user.Id.ToString()),
+            new (ClaimTypes.Role, "Admin"),
+            new ("email", user.Email),
+            new (ClaimTypes.NameIdentifier, user.Name),
+        });
+        return user.ToDto(token);
+    }
+}
--- NN.POS.System.API/Core/Exceptions/Users/UserNotFoundException.cs
+++ NN.POS.System.API/Core/Exceptions/Users/UserNotFoundException.cs
@@ -4,7 +4,7 @@ public class UserNotFoundException : BaseException
 {
     public override string Code => "USER_NOT_FOUND";
 
-    public UserNotFoundException(Guid id) : base($"User with id \"{id}\" not found!")
+    public UserNotFoundException(int id) : base($"User with id \"{id}\" not found!")
     {
         
     }
