96be2d78d29169dd849c4f1d42961cfb64963994
codewithmecoder <yaty1031@gmail.com>
2023-06-24 22:48:43 +0700

    user > delete user

---
+++ NN.POS.System.API/App/Commands/Users/DeleteUserCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace NN.POS.System.API.App.Commands.Users;
+
+public class DeleteUserCommand : IRequest<bool>
+{
+    public int Id { get; set; }
+
+    public DeleteUserCommand(int id)
+    {
+        Id = id;
+    }
+}
