b6126a4d0cb1265ace8364196d5ddc83e49fce9a
codewithmecoder <yaty1031@gmail.com>
2023-06-24 23:27:28 +0700

    role > create role

---
+++ NN.POS.System.API/App/Commands/Roles/Handlers/CreateRoleCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using NN.POS.System.API.Core.Dtos.Roles;
+using NN.POS.System.API.Core.Entities.Roles;
+using NN.POS.System.API.Core.IRepositories.Roles;
+using NN.POS.System.API.Infra.Tables.Roles;
+
+namespace NN.POS.System.API.App.Commands.Roles.Handlers;
+
+public class CreateRoleCommandHandler : IRequestHandler<CreateRoleCommand, RoleDto>
+{
+    private readonly IRoleRepository _roleRepository;
+
+    public CreateRoleCommandHandler(IRoleRepository roleRepository)
+    {
+        _roleRepository = roleRepository;
+    }
+
+    public async Task<RoleDto> Handle(CreateRoleCommand request, CancellationToken cancellationToken)
+    {
+        var roleEn = new RoleEntity(request.Name, DateTime.UtcNow, DateTime.UtcNow)
+        {
+            DisplayName = request.DisplayName,
+            Description = request.Description,
+        };
+        var role = await _roleRepository.CreateRoleAsync(roleEn, cancellationToken);
+        return role.ToDto();
+    }
+}
--- NN.POS.System.API/Controllers/V1/RoleController.cs
+++ NN.POS.System.API/Controllers/V1/RoleController.cs
@@ -1,10 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NN.POS.System.API.App.Commands.Roles;
+using NN.POS.System.API.Core.Dtos.Roles;
 
 namespace NN.POS.System.API.Controllers.V1;
 
 [ApiVersion("1")]
 public class RoleController : BaseApiController
 {
+    private readonly IMediator _mediator;
+
+    public RoleController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
     [HttpGet]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
@@ -15,5 +26,25 @@ public class RoleController : BaseApiController
     public ActionResult Get()
     {
         return Ok(new {Id = 10});
+    }
+
+    [Authorize(Roles = "Admin,write-role")]
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesDefaultResponseType]
+    public async Task<ActionResult<RoleDto>> CreateRole([FromBody] CreateRoleDto b)
+    {
+        var cmd = new CreateRoleCommand(b.Name)
+        {
+            Description = b.Description,
+            DisplayName = b.DisplayName,
+        };
+
+        var data = await _mediator.Send(cmd);
+        return Ok(data);
     }
 }
