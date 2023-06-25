a276b52951f0084556b722c41236b5b5fd678be9
codewithmecoder <yaty1031@gmail.com>
2023-06-25 16:13:22 +0700

    role > create many

---
+++ NN.POS.System.API/App/Commands/Roles/CreateRoleManyCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using NN.POS.System.API.Core.Dtos.Roles;
+
+namespace NN.POS.System.API.App.Commands.Roles;
+
+public class CreateRoleManyCommand : IRequest<bool>
+{
+    public List<CreateRoleDto> Roles { get; set; }
+
+    public CreateRoleManyCommand(List<CreateRoleDto> roles)
+    {
+        Roles = roles;
+    }
+}
---
+++ NN.POS.System.API/App/Commands/Roles/Handlers/CreateRoleManyCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using NN.POS.System.API.Core.Entities.Roles;
+using NN.POS.System.API.Core.IRepositories.Roles;
+
+namespace NN.POS.System.API.App.Commands.Roles.Handlers;
+
+public class CreateRoleManyCommandHandler : IRequestHandler<CreateRoleManyCommand, bool>
+{
+    private readonly IRoleRepository _roleRepository;
+
+    public CreateRoleManyCommandHandler(IRoleRepository roleRepository)
+    {
+        _roleRepository = roleRepository;
+    }
+
+    public Task<bool> Handle(CreateRoleManyCommand request, CancellationToken cancellationToken)
+    {
+        var roleEntities = request.Roles.Select(i =>
+            new RoleEntity(i.Name, DateTime.UtcNow, DateTime.UtcNow)
+            {
+                Description = i.Description,
+                DisplayName = i.DisplayName,
+            }).ToList();
+        return _roleRepository.CreateRoleManyAsync(roleEntities, cancellationToken);
+    }
+}
