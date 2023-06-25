d1f461f7cf12a133748fee0ead38f19cd32039e6
codewithmecoder <yaty1031@gmail.com>
2023-06-25 22:27:05 +0700

    BusinessPartner > update

---
+++ NN.POS.System.API/App/Commands/BusinessPartners/Handlers/UpdateBusinessPartnerCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using MediatR;
+using NN.POS.System.API.Core.Dtos.BusinessPartners;
+using NN.POS.System.API.Core.Entities.BusinessPartners;
+using NN.POS.System.API.Core.IRepositories.BusinessPartners;
+using NN.POS.System.API.Infra.Tables.BusinessPartners;
+
+namespace NN.POS.System.API.App.Commands.BusinessPartners.Handlers;
+
+public class UpdateBusinessPartnerCommandHandler : IRequestHandler<UpdateBusinessPartnerCommand, BusinessPartnerDto>
+{
+    private readonly IBusinessPartnerRepository _businessPartnerRepository;
+
+    public UpdateBusinessPartnerCommandHandler(IBusinessPartnerRepository businessPartnerRepository)
+    {
+        _businessPartnerRepository = businessPartnerRepository;
+    }
+
+    public async Task<BusinessPartnerDto> Handle(UpdateBusinessPartnerCommand r, CancellationToken cancellationToken)
+    {
+        var busEntity = await _businessPartnerRepository.GetByIdAsync(r.Id, cancellationToken);
+
+        var entity = new BusinessPartnerEntity(
+            id: r.Id,
+            firstName: r.FirstName ?? busEntity.FirstName,
+            lastName: r.LastName ?? busEntity.LastName,
+            phoneNumber:r.PhoneNumber ?? busEntity.PhoneNumber,
+            contactType: r.ContactType ?? busEntity.ContactType,
+            businessType: r.BusinessType ?? busEntity.BusinessType,
+            createdAt: busEntity.CreatedAt,
+            updatedAt: DateTime.UtcNow
+            )
+        {
+            Address = r.Address ?? busEntity.Address,
+            Email = r.Email ?? busEntity.Email,
+        };
+        await _businessPartnerRepository.UpdateAsync(entity, cancellationToken);
+        return entity.ToDto();
+    }
+}
---
+++ NN.POS.System.API/App/Commands/BusinessPartners/UpdateBusinessPartnerCommand.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using NN.POS.System.API.Core.Dtos.BusinessPartners;
+using NN.POS.System.API.Core.Enums;
+
+namespace NN.POS.System.API.App.Commands.BusinessPartners;
+
+public class UpdateBusinessPartnerCommand : IRequest<BusinessPartnerDto>
+{
+    public int Id { get; set; }
+    public string? FirstName { get; set; }
+
+    public string? LastName { get; set; }
+
+    public string? PhoneNumber { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? Address { get; set; }
+
+    public BusinessPartnerEnum.ContactType? ContactType { get; set; }
+
+    public BusinessPartnerEnum.BusinessType? BusinessType { get; set; }
+}
