95885a18e87392eb59438fce7cd88fb802237014
codewithmecoder <yaty1031@gmail.com>
2023-06-25 22:51:43 +0700

    BusinessPartner > get by id

---
+++ NN.POS.System.API/App/Queries/BusinessPartners/GetBusinessPartnerByIdQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using NN.POS.System.API.Core.Dtos.BusinessPartners;
+
+namespace NN.POS.System.API.App.Queries.BusinessPartners;
+
+public class GetBusinessPartnerByIdQuery : IRequest<BusinessPartnerDto>
+{
+    public int Id { get; set; }
+
+    public GetBusinessPartnerByIdQuery(int id)
+    {
+        Id = id;
+    }
+}
---
+++ NN.POS.System.API/Infra/QueryHandlers/BusinessPartners/GetBusinessPartnerByIdQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using NN.POS.System.API.App.Queries.BusinessPartners;
+using NN.POS.System.API.Core.Dtos.BusinessPartners;
+using NN.POS.System.API.Core.IRepositories.BusinessPartners;
+using NN.POS.System.API.Infra.Tables.BusinessPartners;
+
+namespace NN.POS.System.API.Infra.QueryHandlers.BusinessPartners;
+
+public class GetBusinessPartnerByIdQueryHandler : IRequestHandler<GetBusinessPartnerByIdQuery, BusinessPartnerDto>
+{
+    private readonly IBusinessPartnerRepository _businessPartnerRepository;
+
+    public GetBusinessPartnerByIdQueryHandler(IBusinessPartnerRepository businessPartnerRepository)
+    {
+        _businessPartnerRepository = businessPartnerRepository;
+    }
+
+    public async Task<BusinessPartnerDto> Handle(GetBusinessPartnerByIdQuery request, CancellationToken cancellationToken)
+    {
+        var data = await _businessPartnerRepository.GetByIdAsync(request.Id, cancellationToken);
+        return data.ToDto();
+    }
+}
