8329b8868c4a33d5a5d0e3ed4bfda5ec4231ef06
codewithmecoder <yaty1031@gmail.com>
2023-06-25 17:00:41 +0700

    role > delete by name

---
+++ NN.POS.System.API/App/Commands/Roles/Handlers/DeleteRoleByNameCommandHandler.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using NN.POS.System.API.Core.IRepositories.Roles;
+
+namespace NN.POS.System.API.App.Commands.Roles.Handlers;
+
+public class DeleteRoleByNameCommandHandler : IRequestHandler<DeleteRoleByNameCommand, bool>
+{
+    private readonly IRoleRepository _roleRepository;
+
+    public DeleteRoleByNameCommandHandler(IRoleRepository roleRepository)
+    {
+        _roleRepository = roleRepository;
+    }
+
+    public Task<bool> Handle(DeleteRoleByNameCommand request, CancellationToken cancellationToken)
+    {
+        return _roleRepository.DeleteRoleAsync(request.Name, cancellationToken);
+    }
+}
