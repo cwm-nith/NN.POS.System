f0c4a8e3544780452edbddf9d8c1324a5f2d2d68
codewithmecoder <yaty1031@gmail.com>
2023-06-30 07:26:43 +0700

    add blazor web ui

---
+++ NN.POS.System.Web/Constants/AppConstants.cs
@@ -0,0 +1,6 @@
+﻿namespace NN.POS.System.Web.Constants;
+
+public class AppConstants
+{
+    public const string HttpClientName = "NN.POS.System";
+}
---
+++ NN.POS.System.Web/Validations/Auth/LoginValidation.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using NN.POS.System.Model.Dtos.Users;
+
+namespace NN.POS.System.Web.Validations.Auth;
+
+public class LoginValidation : AbstractValidator<LoginDto>
+{
+    public LoginValidation()
+    {
+        RuleFor(_ => _.Username)
+            .NotEmpty()
+            .WithMessage("Email required")
+            .EmailAddress()
+            .WithMessage("Invalid Email");
+
+        RuleFor(x => x.Password)
+            .NotEmpty()
+            .WithMessage("Password required");
+    }
+
+    public Func<object, string, Task<IEnumerable<string>>> ValidateValue => async (model, propertyName) =>
+    {
+        var result =
+            await ValidateAsync(ValidationContext<LoginDto>.CreateWithOptions((LoginDto)model, x => x.IncludeProperties(propertyName)));
+        return result.IsValid ? Array.Empty<string>() : result.Errors.Select(e => e.ErrorMessage);
+    };
+}
