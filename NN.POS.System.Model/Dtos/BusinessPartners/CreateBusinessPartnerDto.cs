a7c4c0a9c4fa8dcd03fda236eb6349bdb0adf566
codewithmecoder <yaty1031@gmail.com>
2023-11-18 11:37:02 +0700

    supplier > create

--- NN.POS.System.Model/Dtos/BusinessPartners/CreateBusinessPartnerDto.cs
+++ NN.POS.System.Model/Dtos/BusinessPartners/CreateBusinessPartnerDto.cs
@@ -5,13 +5,8 @@ namespace NN.POS.System.Model.Dtos.BusinessPartners;
 
 public class CreateBusinessPartnerDto : IBaseDto
 {
-    [Required(ErrorMessage = "FirstName field is required")]
     public string FirstName { get; set; } = string.Empty;
-
-    [Required(ErrorMessage = "LastName field is required")]
     public string LastName { get; set; } = string.Empty;
-
-    [Required(ErrorMessage = "PhoneNumber field is required")]
     public string PhoneNumber { get; set; } = string.Empty;
 
     public string? Email { get; set; }
