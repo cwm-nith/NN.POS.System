57e31d2e6ae985778d02f2704f736c80ef7f5325
codewithmecoder <yaty1031@gmail.com>
2023-12-01 00:51:07 +0700

    role > update

---
+++ NN.POS.API/Infra/QueryHandlers/Roles/GetRoleByIdQueryHandler.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using NN.POS.API.App.Queries.Roles;
+using NN.POS.API.Core.IRepositories.Roles;
+using NN.POS.API.Infra.Tables.Roles;
+using NN.POS.Model.Dtos.Roles;
+
+namespace NN.POS.API.Infra.QueryHandlers.Roles;
+
+public class GetRoleByIdQueryHandler(IRoleRepository repository) : IRequestHandler<GetRoleByIdQuery, RoleDto?>
+{
+    public async Task<RoleDto?> Handle(GetRoleByIdQuery request, CancellationToken cancellationToken)
+    {
+        var data = await repository.GetRoleByIdAsync(request.Id, cancellationToken);
+        return data?.ToDto();
+    }
+}
---
+++ NN.POS.Web/Validations/Roles/UpdateRoleValidation.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using NN.POS.Model.Dtos.Roles;
+
+namespace NN.POS.Web.Validations.Roles;
+
+public class UpdateRoleValidator : AbstractValidator<UpdateRoleDto>
+{
+    public UpdateRoleValidator()
+    {
+        RuleFor(i => i.Name)
+            .NotEmpty()
+            .WithMessage("Role name cannot be null!");
+    }
+
+    public Func<object, string, Task<IEnumerable<string>>> ValidateValue => async (model, propertyName) =>
+    {
+        var result =
+            await ValidateAsync(ValidationContext<UpdateRoleDto>.CreateWithOptions((UpdateRoleDto)model, x => x.IncludeProperties(propertyName)));
+        return result.IsValid ? Array.Empty<string>() : result.Errors.Select(e => e.ErrorMessage);
+    };
+}
