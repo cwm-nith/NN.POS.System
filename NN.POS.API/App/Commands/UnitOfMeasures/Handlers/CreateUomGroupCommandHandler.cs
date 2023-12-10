ac88af53b9f345865d14cedac2d01fa292561440
codewithmecoder <yaty1031@gmail.com>
2023-12-10 09:15:58 +0700

    UOMG > create

--- NN.POS.API/App/Commands/UnitOfMeasures/Handlers/CreateUomGroupCommandHandler.cs
+++ NN.POS.API/App/Commands/UnitOfMeasures/Handlers/CreateUomGroupCommandHandler.cs
@@ -1,11 +1,19 @@
 ﻿using MediatR;
+using NN.POS.API.Core.IRepositories.UnitOfMeasures;
+using NN.POS.Model.Dtos.UnitOfMeasures;
 
 namespace NN.POS.API.App.Commands.UnitOfMeasures.Handlers;
 
-public class CreateUomGroupCommandHandler : IRequestHandler<CreateUomGroupCommand>
+public class CreateUomGroupCommandHandler(IUnitOfMeasureGroupRepository repository) : IRequestHandler<CreateUomGroupCommand>
 {
     public async Task Handle(CreateUomGroupCommand request, CancellationToken cancellationToken)
     {
-        
+        await repository.CreateAsync(new UnitOfMeasureGroupDto
+        {
+            CreatedAt = DateTime.UtcNow,
+            Id = 0,
+            IsDeleted = false,
+            Name = request.Name
+        }, cancellationToken);
     }
 }
