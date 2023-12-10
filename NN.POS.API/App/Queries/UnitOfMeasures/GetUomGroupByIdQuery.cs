8c81fa02528e11a4422ba5d903694a5e46a60036
codewithmecoder <yaty1031@gmail.com>
2023-12-10 09:14:37 +0700

    UOMG > get by id

---
+++ NN.POS.API/App/Queries/UnitOfMeasures/GetUomGroupByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using NN.POS.Model.Dtos.UnitOfMeasures;
+
+namespace NN.POS.API.App.Queries.UnitOfMeasures;
+
+public class GetUomGroupByIdQuery(int id) : IRequest<UnitOfMeasureGroupDto>
+{
+    public int Id => id;
+}
---
+++ NN.POS.Model/Dtos/UnitOfMeasures/UnitOfMeasureGroupDto.cs
@@ -0,0 +1,9 @@
+﻿namespace NN.POS.Model.Dtos.UnitOfMeasures;
+
+public class UnitOfMeasureGroupDto : IBaseDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public bool IsDeleted { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
