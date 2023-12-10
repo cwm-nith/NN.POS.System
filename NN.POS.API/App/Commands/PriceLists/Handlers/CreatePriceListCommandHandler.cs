1a6b8a5e2432bab9051e09289f373bb3e8e6bbb0
codewithmecoder <yaty1031@gmail.com>
2023-12-10 12:45:34 +0700

    PL > create

---
+++ NN.POS.API/App/Commands/PriceLists/Handlers/CreatePriceListCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using NN.POS.API.Core.IRepositories.PriceLists;
+using NN.POS.Model.Dtos.PriceLists;
+
+namespace NN.POS.API.App.Commands.PriceLists.Handlers;
+
+public class CreatePriceListCommandHandler(IPriceListRepository repository) : IRequestHandler<CreatePriceListCommand>
+{
+    public async Task Handle(CreatePriceListCommand request, CancellationToken cancellationToken)
+    {
+        var r = request.Dto;
+        await repository.CreateAsync(new PriceListDto
+        {
+            CcyId = r.CcyId,
+            CreatedAt = DateTime.UtcNow,
+            Id = 0,
+            IsDeleted = false,
+            Name = r.Name
+        }, cancellationToken);
+    }
+}
---
+++ NN.POS.API/Controllers/V1/PriceListController.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using NN.POS.API.App.Commands.PriceLists;
+using NN.POS.Model.Dtos.PriceLists;
+
+namespace NN.POS.API.Controllers.V1;
+
+public class PriceListController(IMediator mediator) : BaseApiController
+{
+    #region Price List
+
+    [HttpPost]
+    public async Task<ActionResult> Create([FromBody] CreatePriceListDto body)
+    {
+        await mediator.Send(new CreatePriceListCommand(body));
+        return Ok();
+    }
+
+    #endregion
+}
