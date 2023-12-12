fcc8ba97b6338e769ef8e0cf72dd761328338a51
codewithmecoder <yaty1031@gmail.com>
2023-12-12 22:12:14 +0700

    script > add cg create table

---
+++ Scripts/customer_group.sql
@@ -0,0 +1,24 @@
+USE [NN.POS.System]
+GO
+
+/****** Object:  Table [dbo].[customer_group]    Script Date: 12/12/2023 21:57:29 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[customer_group](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[name] [nvarchar](150) NOT NULL,
+	[value] [decimal](18, 0) NOT NULL,
+	[created_at] [datetime] NOT NULL,
+	[updated_at] [datetime] NOT NULL,
+ CONSTRAINT [name_customer_group] PRIMARY KEY CLUSTERED 
+(
+	[name] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
