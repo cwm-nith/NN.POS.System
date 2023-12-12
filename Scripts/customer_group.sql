f3e27cade9ca37da8e45dbcc78be3e6bdbf7d53f
codewithmecoder <yaty1031@gmail.com>
2023-12-12 22:50:33 +0700

    update script

--- Scripts/customer_group.sql
+++ Scripts/customer_group.sql
@@ -1,24 +1,28 @@
 USE [NN.POS.System]
 GO
 
-/****** Object:  Table [dbo].[customer_group]    Script Date: 12/12/2023 21:57:29 ******/
+/****** Object:  Table [dbo].[customer_groups]    Script Date: 12/12/2023 21:57:29 ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE TABLE [dbo].[customer_group](
+CREATE TABLE [dbo].[customer_groups](
 	[id] [int] IDENTITY(1,1) NOT NULL,
 	[name] [nvarchar](150) NOT NULL,
-	[value] [decimal](18, 0) NOT NULL,
+	[value] [decimal](18, 4) NOT NULL,
 	[created_at] [datetime] NOT NULL,
 	[updated_at] [datetime] NOT NULL,
- CONSTRAINT [name_customer_group] PRIMARY KEY CLUSTERED 
+ CONSTRAINT [name_customer_groups] PRIMARY KEY CLUSTERED 
 (
 	[name] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
+
+CREATE INDEX name_idx_customer_groups
+ON [dbo].[customer_groups] ([name]);
+
 GO
 
 
