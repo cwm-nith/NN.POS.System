a345a424e882e21e40a7180bea25eced690d6b55
codewithmecoder <yaty1031@gmail.com>
2023-12-13 19:26:36 +0700

    enum extension

---
+++ NN.POS.Model/Enums/EnumExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace NN.POS.Model.Enums;
+
+public static class EnumExtensions
+{
+    public static string ToEnumString(this Enum value)
+    {
+        return value.ToString();
+    }
+
+    public static T ToEnum<T>(this string value, bool ignoreCase)
+    {
+        return (T)Enum.Parse(typeof(T), value, ignoreCase);
+    }
+}
