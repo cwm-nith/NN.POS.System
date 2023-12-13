74b3e5a7a5eb0483b4ade90cd069f2090fa2ef45
codewithmecoder <yaty1031@gmail.com>
2023-12-13 19:26:45 +0700

    item db config

---
+++ NN.POS.API/Infra/DbConfigs/ItemMasterDataTableConfig.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NN.POS.API.Infra.Tables.ItemMasters;
+using NN.POS.Model.Enums;
+
+namespace NN.POS.API.Infra.DbConfigs;
+
+public static class ItemMasterDataTableConfig
+{
+    public static ModelBuilder ItemMasterDataTableDbConfig(this ModelBuilder builder)
+    {
+        builder.Entity<ItemMasterDataTable>()
+            .Property(x => x.Process)
+            .HasColumnType("varchar")
+            .HasMaxLength(20)
+            .HasConversion(
+                v => v.ToEnumString(),
+                v => v.ToEnum<ItemMasterDataProcess>(false));
+
+        builder.Entity<ItemMasterDataTable>()
+            .Property(x => x.Type)
+            .HasColumnType("varchar")
+            .HasMaxLength(32)
+            .HasConversion(
+                v => v.ToEnumString(),
+                v => v.ToEnum<ItemMasterDataType>(false));
+
+        return builder;
+    }
+}
