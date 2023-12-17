de27725dd3d16c1c709124ef6553c8afd053c2c6
codewithmecoder <yaty1031@gmail.com>
2023-12-17 17:17:35 +0700

    uomG > add query handler

---
+++ NN.POS.API/Infra/QueryHandlers/UnitOfMeasures/GetUomPageGroupQueryHandler.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using NN.POS.API.App.Queries.UnitOfMeasures;
+using NN.POS.API.Core.IRepositories.UnitOfMeasures;
+using NN.POS.Common.Pagination;
+using NN.POS.Model.Dtos.UnitOfMeasures;
+
+namespace NN.POS.API.Infra.QueryHandlers.UnitOfMeasures;
+
+public class GetUomPageGroupQueryHandler(IUnitOfMeasureGroupRepository repository) : IRequestHandler<GetUomPageGroupQuery, PagedResult<UnitOfMeasureGroupDto>>
+{
+    public async Task<PagedResult<UnitOfMeasureGroupDto>> Handle(GetUomPageGroupQuery request, CancellationToken cancellationToken)
+    {
+        return await repository.GetPageAsync(request, cancellationToken);
+    }
+}
