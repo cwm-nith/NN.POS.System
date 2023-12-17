2341aa86c1153b6df35d0be6ef7a56b1d7179680
codewithmecoder <yaty1031@gmail.com>
2023-12-17 17:39:00 +0700

    uomG > create ui

---
+++ NN.POS.Web/Validations/Uoms/UomGroups/CreateUomGroupValidation.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using NN.POS.Model.Dtos.UnitOfMeasures;
+
+namespace NN.POS.Web.Validations.Uoms.UomGroups;
+
+public class CreateUomGroupValidation : BaseValidator<CreateUomGroupDto>
+{
+    public CreateUomGroupValidation()
+    {
+        RuleFor(i => i.Name)
+            .NotEmpty()
+            .WithMessage("Name is require");
+    }
+}
