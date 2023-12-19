dd40d0a73bd5cb6d20e944c66e3100fd8db57aeb
codewithmecoder <yaty1031@gmail.com>
2023-12-20 01:12:35 +0700

    item > create ui

---
+++ NN.POS.Common/Helpers/ConvertorHelper.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+
+namespace NN.POS.Common.Helpers;
+
+public static class ConvertorHelper
+{
+    public static Stream ConvertToBase64(this Stream stream)
+    {
+        byte[] bytes;
+        using (var memoryStream = new MemoryStream())
+        {
+            stream.CopyTo(memoryStream);
+            bytes = memoryStream.ToArray();
+        }
+
+        var base64 = Convert.ToBase64String(bytes);
+        return new MemoryStream(Encoding.UTF8.GetBytes(base64));
+    }
+}
