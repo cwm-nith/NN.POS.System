36e31e0b5443376b124493482f4eff2a6c7865f3
codewithmecoder <yaty1031@gmail.com>
2023-12-25 19:12:15 +0700

    ccy > add extension convertor

--- NN.POS.API/Infra/Tables/Currencies/CurrencyTable.cs
+++ NN.POS.API/Infra/Tables/Currencies/CurrencyTable.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
+using NN.POS.Model.Dtos.Currencies;
 
 namespace NN.POS.API.Infra.Tables.Currencies;
 
@@ -7,10 +8,31 @@ public class CurrencyTable : BaseTable
 {
     [Column("symbol")]
     public string Symbol { get; set; } = string.Empty;
-    
+
     [Column("Name")]
     public string Name { get; set; } = string.Empty;
 
     [Column("is_deleted")]
     public bool IsDeleted { get; set; }
+}
+
+public static class CurrencyTableExtensions
+{
+    public static CurrencyDto ToDto(this CurrencyTable c) => new()
+    {
+        CreatedAt = c.CreatedAt,
+        Id = c.Id,
+        IsDeleted = c.IsDeleted,
+        Name = c.Name,
+        Symbol = c.Symbol
+    };
+
+    public static CurrencyTable ToTable(this CurrencyDto c) => new()
+    {
+        CreatedAt = c.CreatedAt,
+        Id = c.Id,
+        IsDeleted = c.IsDeleted,
+        Name = c.Name,
+        Symbol = c.Symbol
+    };
 }
