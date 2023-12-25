b931218e6637c122f4860898a4c92bbbd94e41a9
codewithmecoder <yaty1031@gmail.com>
2023-12-25 19:29:04 +0700

    ccy > add repo

---
+++ NN.POS.API/Core/Exceptions/Currencies/CurrencyNotFoundException.cs
@@ -0,0 +1,6 @@
+﻿namespace NN.POS.API.Core.Exceptions.Currencies;
+
+public class CurrencyNotFoundException(int id) : BaseException($"Currency with id \"{id}\" could not be found.")
+{
+    public override string Code => "ccy_nf";
+}
