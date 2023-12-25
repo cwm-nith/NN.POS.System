244a51a566593eec4c3c2d9e0eb572463cb2f54b
codewithmecoder <yaty1031@gmail.com>
2023-12-25 19:08:01 +0700

    ccy > add table

---
+++ NN.POS.API/Infra/Tables/Currencies/CurrencyTable.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NN.POS.API.Infra.Tables.Currencies;
+
+[Table("currencies")]
+public class CurrencyTable : BaseTable
+{
+    [Column("symbol")]
+    public string Symbol { get; set; } = string.Empty;
+    
+    [Column("Name")]
+    public string Name { get; set; } = string.Empty;
+
+    [Column("is_deleted")]
+    public bool IsDeleted { get; set; }
+}
--- NN.POS.API/Infra/Tables/Extensions.cs
+++ NN.POS.API/Infra/Tables/Extensions.cs
@@ -11,6 +11,7 @@ using NN.POS.API.Infra.Repositories.Roles;
 using NN.POS.API.Infra.Repositories.UnitOfMeasures;
 using NN.POS.API.Infra.Repositories.Users;
 using NN.POS.API.Infra.Tables.BusinessPartners;
+using NN.POS.API.Infra.Tables.Currencies;
 using NN.POS.API.Infra.Tables.ItemMasters;
 using NN.POS.API.Infra.Tables.PriceLists;
 using NN.POS.API.Infra.Tables.Roles;
@@ -34,6 +35,7 @@ public static class Extensions
         services.AddPostgresRepository<PriceListTable>();
         services.AddPostgresRepository<PriceListDetailTable>();
         services.AddPostgresRepository<ItemMasterDataTable>();
+        services.AddPostgresRepository<CurrencyTable>();
 
         services.AddScoped(typeof(DataDbContext),
           sp =>
