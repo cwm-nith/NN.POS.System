860b84b2be0abead7d024c605b795c24cd5ccae6
codewithmecoder <yaty1031@gmail.com>
2023-12-25 19:18:52 +0700

    ccy > setup repo

---
+++ NN.POS.API/Core/IRepositories/Currencies/ICurrencyRepository.cs
@@ -0,0 +1,14 @@
+﻿using NN.POS.API.App.Queries.Currencies;
+using NN.POS.Common.Pagination;
+using NN.POS.Model.Dtos.Currencies;
+
+namespace NN.POS.API.Core.IRepositories.Currencies;
+
+public interface ICurrencyRepository : IRepository
+{
+    Task CreateAsync(CurrencyDto ccy, CancellationToken cancellationToken= default);
+    Task UpdateAsync(CurrencyDto ccy, CancellationToken cancellationToken= default);
+    Task DeleteAsync(int id, CancellationToken cancellationToken = default);
+    Task<CurrencyDto> GetByIdAsync(int id, CancellationToken cancellationToken = default);
+    Task<PagedResult<CurrencyDto>> GetPageAsync(GetCurrenciesPageQuery query, CancellationToken cancellationToken= default);
+}
---
+++ NN.POS.API/Infra/Repositories/Currencies/CurrencyRepository.cs
@@ -0,0 +1,34 @@
+﻿using NN.POS.API.App.Queries.Currencies;
+using NN.POS.API.Core.IRepositories.Currencies;
+using NN.POS.Common.Pagination;
+using NN.POS.Model.Dtos.Currencies;
+
+namespace NN.POS.API.Infra.Repositories.Currencies;
+
+public class CurrencyRepository : ICurrencyRepository
+{
+    public Task CreateAsync(CurrencyDto ccy, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(CurrencyDto ccy, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteAsync(int id, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<CurrencyDto> GetByIdAsync(int id, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<PagedResult<CurrencyDto>> GetPageAsync(GetCurrenciesPageQuery query, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+}
