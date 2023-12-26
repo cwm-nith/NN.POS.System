daaf753f251ced088267f6076a28f78c44098a3b
codewithmecoder <yaty1031@gmail.com>
2023-12-26 20:07:14 +0700

    wh> add table

---
+++ NN.POS.API/Infra/Tables/Warehouses/WarehouseTable.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NN.POS.API.Infra.Tables.Warehouses;
+
+[Table("warehouses")]
+public class WarehouseTable : BaseTable
+{
+    [Column("branch_id")]
+    public int BranchId { get; set; }
+
+    [Column("code")]
+    public string Code { get; set; } = string.Empty;
+
+    [Column("name")]
+    public string Name { get; set; } = string.Empty;
+
+    [Column("stock_in", TypeName = "decimal(18,4)")]
+    public decimal StockIn { get; set; }
+
+    [Column("location")]
+    public string? Location { get; set; }
+
+    [Column("address")]
+    public string? Address { get; set; }
+
+    [Column("is_deleted")]
+    public bool IsDeleted { get; set; }
+}
