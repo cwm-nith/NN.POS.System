697c19e010315ce00c93290ed865336f9c2cdb41
codewithmecoder <yaty1031@gmail.com>
2023-12-27 07:36:04 +0700

    ws > get page api

---
+++ NN.POS.API/Controllers/V1/WarehouseController.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using NN.POS.API.App.Queries.Warehouses;
+using NN.POS.Common.Pagination;
+using NN.POS.Model.Dtos.Warehouses;
+
+namespace NN.POS.API.Controllers.V1;
+
+public class WarehouseController(IMediator mediator) : BaseApiController
+{
+    [HttpGet]
+    public async Task<ActionResult<PagedResult<WarehouseDto>>> GetPage([FromQuery] GetPageWarehouseDto q)
+    {
+        var data = await mediator.Send(new GetPageWarehouseQuery
+        {
+            Page = q.Page,
+            Results = q.Results,
+            Search = q.Search
+        });
+        return Ok(data);
+    }
+}
