99c24cc7a30737a6d9d8e7e227ef41e6e207421a
codewithmecoder <yaty1031@gmail.com>
2023-12-27 19:07:07 +0700

    com > get page

---
+++ NN.POS.API/Controllers/V1/CompanyController.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using NN.POS.API.App.Queries.Company;
+using NN.POS.Common.Pagination;
+using NN.POS.Model.Dtos.Company;
+
+namespace NN.POS.API.Controllers.V1;
+
+public class CompanyController(IMediator mediator) : BaseApiController
+{
+    [HttpGet]
+    public async Task<ActionResult<PagedResult<CompanyDto>>> GetPage([FromQuery] GetCompanyPageDto q)
+    {
+        var data = await mediator.Send(new GetCompanyPageQuery
+        {
+            Page = q.Page,
+            Results = q.Results,
+            Search = q.Search
+        });
+        return Ok(data);
+    }
+}
---
+++ NN.POS.API/Infra/QueryHandlers/Company/GetCompanyPageQueryHandler.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using NN.POS.API.App.Queries.Company;
+using NN.POS.API.Core.IRepositories.Company;
+using NN.POS.Common.Pagination;
+using NN.POS.Model.Dtos.Company;
+
+namespace NN.POS.API.Infra.QueryHandlers.Company;
+
+public class GetCompanyPageQueryHandler(ICompanyRepository repository) : IRequestHandler<GetCompanyPageQuery, PagedResult<CompanyDto>>
+{
+    public async Task<PagedResult<CompanyDto>> Handle(GetCompanyPageQuery request, CancellationToken cancellationToken)
+    {
+        return await repository.GetPageAsync(request, cancellationToken);
+    }
+}
