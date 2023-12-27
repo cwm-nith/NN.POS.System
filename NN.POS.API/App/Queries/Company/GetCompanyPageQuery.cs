4ac6580ff2609a4961b93a5fa5f3ce5e8c9b9ddd
codewithmecoder <yaty1031@gmail.com>
2023-12-27 19:02:27 +0700

    com > add repo

---
+++ NN.POS.API/App/Queries/Company/GetCompanyPageQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using NN.POS.Common.Pagination;
+using NN.POS.Model.Dtos.Company;
+
+namespace NN.POS.API.App.Queries.Company;
+
+public class GetCompanyPageQuery : PagedQuery, IRequest<PagedResult<CompanyDto>>
+{
+    public string? Search { get; set; }
+}
---
+++ NN.POS.API/Core/IRepositories/Company/ICompanyRepository.cs
@@ -0,0 +1,14 @@
+﻿using NN.POS.API.App.Queries.Company;
+using NN.POS.Common.Pagination;
+using NN.POS.Model.Dtos.Company;
+
+namespace NN.POS.API.Core.IRepositories.Company;
+
+public interface ICompanyRepository : IRepository
+{
+    Task CreateAsync(CompanyDto body, CancellationToken cancellationToken = default);
+    Task UpdateAsync(CompanyDto body, CancellationToken cancellationToken = default);
+    Task DeleteAsync(int id, CancellationToken cancellationToken = default);
+    Task<CompanyDto> GetByIdAsync(int id, CancellationToken cancellationToken = default);
+    Task<PagedResult<CompanyDto>> GetPageAsync(GetCompanyPageQuery query, CancellationToken cancellationToken = default);
+}
