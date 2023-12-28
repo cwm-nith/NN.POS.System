42dd57f594ea946b89dc8e90bf7ea6e4eff6b840
codewithmecoder <yaty1031@gmail.com>
2023-12-28 19:40:56 +0700

    pl > create ui

--- NN.POS.Model/Dtos/PriceLists/CreatePriceListDto.cs
+++ NN.POS.Model/Dtos/PriceLists/CreatePriceListDto.cs
@@ -3,6 +3,5 @@
 public class CreatePriceListDto : IBaseDto
 {
     public string Name { get; set; } = string.Empty;
-    public bool IsDeleted { get; set; }
     public int CcyId { get; set; }
 }
---
+++ NN.POS.Web/Helpers/ErrorApiHelper.cs
@@ -0,0 +1,38 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using NN.POS.Model.Dtos;
+
+namespace NN.POS.Web.Helpers;
+
+public static class ErrorApiHelper
+{
+    public static async Task<string> GetErrorMessageResponse(
+        HttpResponseMessage response,
+        Action? unauthorized = null,
+        Action? ok = null)
+    {
+        switch (response.StatusCode)
+        {
+            case HttpStatusCode.BadRequest:
+                {
+                    var errors = await response.Content.ReadFromJsonAsync<ErrorResponse>();
+
+                    var code = !string.IsNullOrEmpty(errors?.Code) ? $" [{errors.Code}]" : "";
+
+                    return $"{errors?.Message}{code}";
+                }
+            case HttpStatusCode.Unauthorized:
+                {
+                    unauthorized?.Invoke();
+                    return "";
+                }
+            case HttpStatusCode.OK:
+                {
+                    ok?.Invoke();
+                    return "";
+                }
+            default:
+                return "Something went wrong, Please try after some time";
+        }
+    }
+}
