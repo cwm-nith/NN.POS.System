faca87bd5264c5cae41c1e9a70543b01c31939a5
codewithmecoder <yaty1031@gmail.com>
2023-12-29 18:59:29 +0700

    file upload

--- NN.POS.Model/Dtos/Company/UpdateCompanyLogoDto.cs
+++ NN.POS.Model/Dtos/Company/UpdateCompanyLogoDto.cs
@@ -1,9 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
-
-namespace NN.POS.Model.Dtos.Company;
+﻿namespace NN.POS.Model.Dtos.Company;
 
 public class UpdateCompanyLogoDto : IBaseDto
 {
     public string Logo { get; set; } = string.Empty;
-    public IFormFile? LogoFile { get; set; }
+    public FileData? LogoFile { get; set; }
 }
