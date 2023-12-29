f745f53b71083f32c53eb1a389e1343af04c2593
codewithmecoder <yaty1031@gmail.com>
2023-12-29 18:59:40 +0700

    com > create and listing

---
+++ NN.POS.Web/Validations/Company/CreateCompanyValidation.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+using NN.POS.Model.Dtos.Company;
+
+namespace NN.POS.Web.Validations.Company;
+
+public class CreateCompanyValidation : BaseValidator<CreateCompanyDto>
+{
+    public CreateCompanyValidation()
+    {
+        RuleFor(i => i.LocalCcyId)
+            .NotEmpty()
+            .WithMessage("Please choose Local Currency")
+            .Must(i => i > 0)
+            .WithMessage("Please choose Local Currency");
+
+        RuleFor(i => i.SysCcyId)
+            .NotEmpty()
+            .WithMessage("Please choose System Currency")
+            .Must(i => i > 0)
+            .WithMessage("Please choose System Currency");
+
+        RuleFor(i => i.PriceListId)
+            .NotEmpty()
+            .WithMessage("Please choose Price List")
+            .Must(i => i > 0)
+            .WithMessage("Please choose Price List");
+
+        RuleFor(i => i.Name)
+            .NotEmpty()
+            .WithMessage("Please input name");
+    }
+}
