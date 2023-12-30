7954690831da8a2ba762340b833cb865a9e60d60
codewithmecoder <yaty1031@gmail.com>
2023-12-30 19:09:56 +0700

    tax > add repo

---
+++ NN.POS.API/Core/Exceptions/Tax/TaxNotFoundException.cs
@@ -0,0 +1,6 @@
+﻿namespace NN.POS.API.Core.Exceptions.Tax;
+
+public class TaxNotFoundException(int id) : BaseException($"Tax with id \"{id}\" could not be found")
+{
+    public override string Code => "tax_nf";
+}
---
+++ NN.POS.API/Core/IRepositories/Tax/ITaxRepository.cs
@@ -0,0 +1,14 @@
+﻿using NN.POS.API.App.Queries.Tax;
+using NN.POS.Common.Pagination;
+using NN.POS.Model.Dtos.Tax;
+
+namespace NN.POS.API.Core.IRepositories.Tax;
+
+public interface ITaxRepository : IRepository
+{
+    Task CreateAsync(TaxDto body, CancellationToken cancellationToken = default);
+    Task UpdateAsync(TaxDto body, CancellationToken cancellationToken = default);
+    Task DeleteAsync(int id, CancellationToken cancellationToken = default);
+    Task<TaxDto> GetByIdAsync(int id, CancellationToken cancellationToken = default);
+    Task<PagedResult<TaxDto>> GetPageAsync(GetTaxPageQuery query, CancellationToken cancellationToken = default);
+}
