08efbb57284d238cfda8237097552f688fd7e57e
codewithmecoder <yaty1031@gmail.com>
2023-12-30 19:12:39 +0700

    tax > get page

---
+++ NN.POS.API/Controllers/V1/TaxController.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using NN.POS.API.App.Queries.Tax;
+using NN.POS.Common.Pagination;
+using NN.POS.Model.Dtos.Tax;
+
+namespace NN.POS.API.Controllers.V1;
+
+public class TaxController(IMediator mediator) : BaseApiController
+{
+    [HttpGet]
+    public async Task<ActionResult<PagedResult<TaxDto>>> GetPage([FromQuery] GetTaxPageQuery query)
+    {
+        var data = await mediator.Send(query);
+        return Ok(data);
+    }
+}
