e3e58a8a345f0e6733ece4689f1e47353545f9bf
codewithmecoder <yaty1031@gmail.com>
2023-12-30 08:45:30 +0700

    br > validation

---
+++ NN.POS.Web/Validations/Branches/CreateUpdateBranchValidation.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using NN.POS.Model.Dtos.Company.Branches;
+
+namespace NN.POS.Web.Validations.Branches;
+
+public class CreateUpdateBranchValidation : BaseValidator<CreateBranchDto>
+{
+    public CreateUpdateBranchValidation()
+    {
+        RuleFor(i => i.Address)
+            .NotEmpty()
+            .WithMessage("Please input address");
+
+        RuleFor(i => i.Location)
+            .NotEmpty()
+            .WithMessage("Please input location");
+
+        RuleFor(i => i.Name)
+            .NotEmpty()
+            .WithMessage("Please input name");
+
+        RuleFor(i => i.CompanyId)
+            .NotEmpty()
+            .WithMessage("Please choose company")
+            .Must(i => i > 0)
+            .WithMessage("Please choose company");
+    }
+}
