5e4eb09756b85e6ee0ac8146ff47ecbcbb51e09f
codewithmecoder <yaty1031@gmail.com>
2023-12-31 14:54:02 +0700

    ex_rate > get page

---
+++ NN.POS.API/Controllers/V1/ExchangeRateController.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using NN.POS.API.App.Queries.Currencies;
+using NN.POS.Common.Pagination;
+using NN.POS.Model.Dtos.Currencies;
+
+namespace NN.POS.API.Controllers.V1;
+
+public class ExchangeRateController(IMediator mediator) : BaseApiController
+{
+    [HttpGet]
+    public async Task<ActionResult<PagedResult<ExchangeRateDto>>> GetPage([FromQuery] GetExchangeRatePageQuery query)
+    {
+        var data = await mediator.Send(query);
+        return Ok(data);
+    }
+}
