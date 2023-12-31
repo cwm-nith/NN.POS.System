5e7573ccc0760bb973e84c5b27fd5929c1c1f187
codewithmecoder <yaty1031@gmail.com>
2023-12-31 14:37:26 +0700

    ex_rate > add extensions

--- NN.POS.API/Infra/Tables/Currencies/ExchangeRateTable.cs
+++ NN.POS.API/Infra/Tables/Currencies/ExchangeRateTable.cs
@@ -1,20 +1,43 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
+using NN.POS.Model.Dtos.Currencies;
 
-namespace NN.POS.API.Infra.Tables.Currencies
+namespace NN.POS.API.Infra.Tables.Currencies;
+
+[Table("exchange_rates")]
+public class ExchangeRateTable : BaseTable
 {
-    [Table("exchange_rates")]
-    public class ExchangeRateTable : BaseTable
+    [Column("ccy_id")]
+    public int CcyId { get; set; }
+
+    [Column("rate", TypeName = "decimal(18,7)")]
+    public decimal Rate { get; set; }
+
+    [Column("is_deleted")]
+    public bool IsDeleted { get; set; }
+
+    [Column("set_rate", TypeName = "decimal(18,7)")]
+    public decimal SetRate { get; set; }
+}
+
+public static class ExchangeRateExtensions
+{
+    public static ExchangeRateDto ToDto(this ExchangeRateTable e, string? ccyName = null) => new()
     {
-        [Column("ccy_id")]
-        public int CcyId { get; set; }
-
-        [Column("rate", TypeName = "decimal(18,7)")]
-        public decimal Rate { get; set; }
-
-        [Column("is_deleted")]
-        public bool IsDeleted { get; set; }
-
-        [Column("set_rate", TypeName = "decimal(18,7)")]
-        public decimal SetRate { get; set; }
-    }
+        CreatedAt = e.CreatedAt,
+        CcyId = e.CcyId,
+        Id = e.Id,
+        IsDeleted = e.IsDeleted,
+        Rate = e.Rate,
+        SetRate = e.SetRate,
+        CcyName = ccyName
+    };
+    public static ExchangeRateTable ToTable(this ExchangeRateDto e) => new()
+    {
+        CreatedAt = e.CreatedAt,
+        CcyId = e.CcyId,
+        Id = e.Id,
+        IsDeleted = e.IsDeleted,
+        Rate = e.Rate,
+        SetRate = e.SetRate
+    };
 }
---
+++ NN.POS.Model/Dtos/Currencies/ExchangeRateDto.cs
@@ -0,0 +1,12 @@
+﻿namespace NN.POS.Model.Dtos.Currencies;
+
+public class ExchangeRateDto : IBaseDto
+{
+    public int Id { get; set; }
+    public int CcyId { get; set; }
+    public string? CcyName { get; set; }
+    public decimal Rate { get; set; }
+    public bool IsDeleted { get; set; }
+    public decimal SetRate { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
