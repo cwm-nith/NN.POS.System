9524ac8e63b799d11317a4c67fd31249a49c909a
codewithmecoder <yaty1031@gmail.com>
2023-12-31 10:45:23 +0700

    tax > create

---
+++ NN.POS.Web/Validations/Tax/CreateUpdateTaxValidation.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+using NN.POS.Model.Dtos.Tax;
+using NN.POS.Model.Enums;
+
+namespace NN.POS.Web.Validations.Tax;
+
+public class CreateUpdateTaxValidation : BaseValidator<CreateTaxDto>
+{
+    public CreateUpdateTaxValidation()
+    {
+        RuleFor(i => i.EffectiveDate)
+            .NotEmpty()
+            .WithMessage("Please select Effective Date")
+            .Must(i=> i > DateTime.Now.Date)
+            .WithMessage("Effective Date must be greater than current date");
+
+        RuleFor(i => i.Name)
+            .NotEmpty()
+            .WithMessage("Please input name");
+
+        RuleFor(i => i.Rate)
+            .NotEmpty()
+            .WithMessage("Please input rate")
+            .Must(i => i > 0)
+            .WithMessage("Rate must be greater than zero");
+
+        RuleFor(i => i.Type)
+            .Must(i => i != TaxType.None)
+            .WithMessage("Please select Rate Type");
+    }
+}
