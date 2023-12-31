00a9540b74263ba398bc972858c2a203f05254ba
codewithmecoder <yaty1031@gmail.com>
2023-12-31 11:05:59 +0700

    tax < [EnumMember]

--- NN.POS.Model/Enums/TaxType.cs
+++ NN.POS.Model/Enums/TaxType.cs
@@ -1,8 +1,15 @@
-﻿namespace NN.POS.Model.Enums;
+﻿using System.Runtime.Serialization;
+
+namespace NN.POS.Model.Enums;
 
 public enum TaxType
 {
+    [EnumMember(Value = "NONE")]
     None = 0,
+
+    [EnumMember(Value = "FLAT")]
     Flat = 1,
+
+    [EnumMember(Value = "PERCENTAGE")]
     Percentage = 2
 }
