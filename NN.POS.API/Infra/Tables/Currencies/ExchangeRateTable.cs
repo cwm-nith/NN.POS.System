05a9ca96bc244bc795e8f99401b02d424bb158a5
codewithmecoder <yaty1031@gmail.com>
2023-12-31 14:32:34 +0700

    ex_rate > setup table

---
+++ NN.POS.API/Infra/Tables/Currencies/ExchangeRateTable.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NN.POS.API.Infra.Tables.Currencies
+{
+    [Table("exchange_rates")]
+    public class ExchangeRateTable : BaseTable
+    {
+        [Column("ccy_id")]
+        public int CcyId { get; set; }
+
+        [Column("rate", TypeName = "decimal(18,7)")]
+        public decimal Rate { get; set; }
+
+        [Column("is_deleted")]
+        public bool IsDeleted { get; set; }
+
+        [Column("set_rate", TypeName = "decimal(18,7)")]
+        public decimal SetRate { get; set; }
+    }
+}
