78e2739abe2f805c928cca273d0939674155cea3
codewithmecoder <yaty1031@gmail.com>
2024-01-01 15:11:18 +0700

    pmt > add repo

---
+++ NN.POS.API/App/Queries/PaymentTypes/GetPaymentTypePageQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using NN.POS.Common.Pagination;
+using NN.POS.Model.Dtos.PaymentTypes;
+
+namespace NN.POS.API.App.Queries.PaymentTypes;
+
+public class GetPaymentTypePageQuery : PagedQuery, IRequest<PagedResult<PaymentTypeDto>>
+{
+    public string? Search { get; set; }
+}
