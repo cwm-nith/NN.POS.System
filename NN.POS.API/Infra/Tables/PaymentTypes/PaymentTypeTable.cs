5004bc5b3dd73a943e6fa972cb760391f67340d3
codewithmecoder <yaty1031@gmail.com>
2024-01-01 15:01:17 +0700

    paymentType > add extensions

--- NN.POS.API/Infra/Tables/PaymentTypes/PaymentTypeTable.cs
+++ NN.POS.API/Infra/Tables/PaymentTypes/PaymentTypeTable.cs
@@ -1,12 +1,33 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using NN.POS.Model.Dtos.PaymentTypes;
 
 namespace NN.POS.API.Infra.Tables.PaymentTypes;
 
 [Table("payment_types")]
 public class PaymentTypeTable : BaseTable
 {
-    [Column("name")]
+    [Column("name"), StringLength(50)]
     public string Name { get; set; } = string.Empty;
     [Column("is_deleted")] 
     public bool IsDeleted { get; set; }
+}
+
+public static class PaymentTypeExtensions
+{
+    public static PaymentTypeDto ToDto(this PaymentTypeTable p) => new()
+    {
+        CreatedAt = p.CreatedAt,
+        Id = p.Id,
+        IsDeleted = p.IsDeleted,
+        Name = p.Name
+    };
+
+    public static PaymentTypeTable ToTable(this PaymentTypeDto p) => new()
+    {
+        CreatedAt = p.CreatedAt,
+        Id = p.Id,
+        IsDeleted = p.IsDeleted,
+        Name = p.Name
+    };
 }
---
+++ NN.POS.Model/Dtos/PaymentTypes/PaymentTypeDto.cs
@@ -0,0 +1,9 @@
+﻿namespace NN.POS.Model.Dtos.PaymentTypes;
+
+public class PaymentTypeDto : IBaseDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public bool IsDeleted { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
