d0a523ba0cdce861800a9b605dedca7401a15eb3
codewithmecoder <yaty1031@gmail.com>
2024-01-03 21:58:34 +0700

    pur_order > get page

---
+++ NN.POS.API/Controllers/V1/PurchaseOrderController.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using NN.POS.API.App.Queries.Purchases;
+using NN.POS.Common.Pagination;
+using NN.POS.Model.Dtos.Purchases.PurchaseOrders;
+
+namespace NN.POS.API.Controllers.V1;
+
+public class PurchaseOrderController(IMediator mediator) : BaseApiController
+{
+    [HttpGet]
+    public async Task<ActionResult<PagedResult<PurchaseOrderDto>>> GetPage([FromQuery] GetPurchaseOrderPageQuery query)
+    {
+        return Ok(await mediator.Send(query));
+    }
+}
