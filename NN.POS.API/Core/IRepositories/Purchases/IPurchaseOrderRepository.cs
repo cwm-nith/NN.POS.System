62951cff6f1fc5a41e8dd708db0e6e425cd11bee
codewithmecoder <yaty1031@gmail.com>
2024-01-03 21:54:19 +0700

    pur_order > add repo

--- NN.POS.API/Core/IRepositories/Purchases/IPurchaseOrderRepository.cs
+++ NN.POS.API/Core/IRepositories/Purchases/IPurchaseOrderRepository.cs
@@ -1,4 +1,5 @@
-﻿using NN.POS.Common.Pagination;
+﻿using NN.POS.API.App.Queries.Purchases;
+using NN.POS.Common.Pagination;
 using NN.POS.Model.Dtos.Purchases.PurchaseOrders;
 
 namespace NN.POS.API.Core.IRepositories.Purchases;
@@ -9,5 +10,5 @@ public interface IPurchaseOrderRepository : IRepository
     Task Update(PurchaseOrderDto body, CancellationToken cancellationToken = default);
     Task<PurchaseOrderDto> GetByInvoiceNoAsync(string invoiceNo, CancellationToken cancellationToken = default);
     Task<PurchaseOrderDto> GetByIdAsync(int id, CancellationToken cancellationToken = default);
-    Task<PagedResult<PurchaseOrderDto>> GetPageAsync(int id, CancellationToken cancellationToken = default);
+    Task<PagedResult<PurchaseOrderDto>> GetPageAsync(GetPurchaseOrderPageQuery query, CancellationToken cancellationToken = default);
 }
--- NN.POS.API/Infra/Repositories/Purchases/PurchaseOrderRepository.cs
+++ NN.POS.API/Infra/Repositories/Purchases/PurchaseOrderRepository.cs
@@ -1,8 +1,12 @@
-﻿using NN.POS.API.Core.IRepositories.Purchases;
+﻿using Microsoft.EntityFrameworkCore;
+using NN.POS.API.App.Queries.Purchases;
+using NN.POS.API.Core.Exceptions.Purchases;
+using NN.POS.API.Core.IRepositories.Purchases;
 using NN.POS.API.Infra.Tables;
 using NN.POS.API.Infra.Tables.Purchases.PurchaseOrders;
 using NN.POS.Common.Pagination;
 using NN.POS.Model.Dtos.Purchases.PurchaseOrders;
+using NN.POS.Model.Enums;
 
 namespace NN.POS.API.Infra.Repositories.Purchases;
 
@@ -23,16 +27,77 @@ public class PurchaseOrderRepository(
     public async Task<PurchaseOrderDto> GetByInvoiceNoAsync(string invoiceNo, CancellationToken cancellationToken = default)
     {
         var context = readDbRepository.Context;
-        return default;
+        var data = await (from pOrder in context.PurchaseOrders!
+                .Include(i => i.PurchaseOrderDetails)
+                .Where(i => i.Status == PurchaseStatus.Open && i.InvoiceNo == invoiceNo)
+                          join br in context.Branches! on pOrder.BranchId equals br.Id
+                          join supplier in context.BusinessPartners! on pOrder.SupplyId equals supplier.Id
+                          join sysCcy in context.Currencies! on pOrder.SysCcyId equals sysCcy.Id
+                          join localCcy in context.Currencies! on pOrder.LocalCcyId equals localCcy.Id
+                          join ws in context.Warehouses! on pOrder.WarehouseId equals ws.Id
+                          join purCcy in context.Currencies! on pOrder.PurCcyId equals purCcy.Id
+                          join user in context.Users! on pOrder.UserId equals user.Id
+                          select pOrder.ToDto(
+                              branchName: br.Name,
+                              supplyName: supplier.ToString(),
+                              sysCcyName: sysCcy.Name,
+                              localCcyName: localCcy.Name,
+                              wsName: ws.Name,
+                              purCcyName: purCcy.Name,
+                              userName: user.Name)).FirstOrDefaultAsync(cancellationToken);
+        return data ?? throw new PurchaseOrderNotFoundException(invoiceNo);
     }
 
-    public Task<PurchaseOrderDto> GetByIdAsync(int id, CancellationToken cancellationToken = default)
+    public async Task<PurchaseOrderDto> GetByIdAsync(int id, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        var context = readDbRepository.Context;
+        var data = await (from pOrder in context.PurchaseOrders!
+                .Include(i => i.PurchaseOrderDetails)
+                .Where(i => i.Status == PurchaseStatus.Open && i.Id == id)
+                          join br in context.Branches! on pOrder.BranchId equals br.Id
+                          join supplier in context.BusinessPartners! on pOrder.SupplyId equals supplier.Id
+                          join sysCcy in context.Currencies! on pOrder.SysCcyId equals sysCcy.Id
+                          join localCcy in context.Currencies! on pOrder.LocalCcyId equals localCcy.Id
+                          join ws in context.Warehouses! on pOrder.WarehouseId equals ws.Id
+                          join purCcy in context.Currencies! on pOrder.PurCcyId equals purCcy.Id
+                          join user in context.Users! on pOrder.UserId equals user.Id
+                          select pOrder.ToDto(
+                              branchName: br.Name,
+                              supplyName: supplier.ToString(),
+                              sysCcyName: sysCcy.Name,
+                              localCcyName: localCcy.Name,
+                              wsName: ws.Name,
+                              purCcyName: purCcy.Name,
+                              userName: user.Name)).FirstOrDefaultAsync(cancellationToken);
+        return data ?? throw new PurchaseOrderNotFoundException(id);
     }
 
-    public Task<PagedResult<PurchaseOrderDto>> GetPageAsync(int id, CancellationToken cancellationToken = default)
+    public async Task<PagedResult<PurchaseOrderDto>> GetPageAsync(GetPurchaseOrderPageQuery query, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        var context = readDbRepository.Context;
+
+        var data = await (from pOrder in context.PurchaseOrders!
+        .Include(i => i.PurchaseOrderDetails)
+                .Where(i =>
+            (query.PurchaseStatus == null || i.Status == query.PurchaseStatus) &&
+            (query.FromDate == null || query.ToDate == null || i.PostingDate >= query.FromDate && i.PostingDate <= query.ToDate) &&
+            EF.Functions.Like(i.InvoiceNo, $"%{query.Search}%"))
+
+                          join br in context.Branches! on pOrder.BranchId equals br.Id
+                          join supplier in context.BusinessPartners! on pOrder.SupplyId equals supplier.Id
+                          join sysCcy in context.Currencies! on pOrder.SysCcyId equals sysCcy.Id
+                          join localCcy in context.Currencies! on pOrder.LocalCcyId equals localCcy.Id
+                          join ws in context.Warehouses! on pOrder.WarehouseId equals ws.Id
+                          join purCcy in context.Currencies! on pOrder.PurCcyId equals purCcy.Id
+                          join user in context.Users! on pOrder.UserId equals user.Id
+                          select pOrder.ToDto(
+                              branchName: br.Name,
+                              supplyName: supplier.ToString(),
+                              sysCcyName: sysCcy.Name,
+                              localCcyName: localCcy.Name,
+                              wsName: ws.Name,
+                              purCcyName: purCcy.Name,
+                              userName: user.Name)).PaginateAsync(query, cancellationToken);
+        return data;
     }
 }
