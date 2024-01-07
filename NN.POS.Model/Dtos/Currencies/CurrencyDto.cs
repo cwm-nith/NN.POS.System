dd2b3390ab78a25598d183d9ac75ff3cbad87491
codewithmecoder <yaty1031@gmail.com>
2024-01-07 12:25:44 +0700

    update > ccy repo

--- NN.POS.Model/Dtos/Currencies/CurrencyDto.cs
+++ NN.POS.Model/Dtos/Currencies/CurrencyDto.cs
@@ -5,6 +5,7 @@ public class CurrencyDto : IBaseDto
     public int Id { get; set; }
     public string Symbol { get; set; } = string.Empty;
     public string Name { get; set; } = string.Empty;
+    public ExchangeRateDto? ExchangeRate { get; set; }
     public bool IsDeleted { get; set; }
     public DateTime CreatedAt { get; set; }
 }
