5afb688776b9312320e1a1b95adb7da26173d95e
codewithmecoder <yaty1031@gmail.com>
2024-01-13 18:52:41 +0700

    pld > fix create

--- NN.POS.API/App/Commands/PriceLists/Handlers/CreatePriceListDetailCommandHandler.cs
+++ NN.POS.API/App/Commands/PriceLists/Handlers/CreatePriceListDetailCommandHandler.cs
@@ -1,26 +1,57 @@
 ﻿using MediatR;
+using NN.POS.API.Core.IRepositories.ItemMasters;
 using NN.POS.API.Core.IRepositories.PriceLists;
+using NN.POS.API.Core.IRepositories.UnitOfMeasures;
 using NN.POS.Model.Dtos.PriceLists;
 
 namespace NN.POS.API.App.Commands.PriceLists.Handlers;
 
-public class CreatePriceListDetailCommandHandler(IPriceListDetailRepository repository) : IRequestHandler<CreatePriceListDetailCommand>
+public class CreatePriceListDetailCommandHandler(
+    IPriceListDetailRepository repository, 
+    IItemMasterDataRepository itemRepository,
+    IUnitOfMeasureDefineRepository uomDefineRepository) : IRequestHandler<CreatePriceListDetailCommand>
 {
     public async Task Handle(CreatePriceListDetailCommand request, CancellationToken cancellationToken)
     {
-        await repository.CreateAsync(request.Dto.Select(i => new PriceListDetailDto
+        var plds = new List<PriceListDetailDto>();
+
+        foreach (var pld in request.Dto)
         {
-            CcyId = i.CcyId,
-            Price = i.Price,
-            Cost = i.Cost,
-            CreatedAt = DateTime.UtcNow,
-            DiscountType = i.DiscountType,
-            DiscountValue = i.DiscountValue,
-            Id = 0,
-            ItemId = i.ItemId,
-            PriceListId = i.PriceListId,
-            PromotionId = i.PromotionId,
-            UomId = i.UomId
-        }).ToList(), cancellationToken);
+            var item = await itemRepository.GetByIdAsync(pld.ItemId, cancellationToken);
+            var uomDefines = (await uomDefineRepository.GetUomDefineByGroupIdAsync(item.UomGroupId))
+                .Where(i => i.AltUomId != pld.UomId).ToList();
+
+            plds.Add(new PriceListDetailDto
+            {
+                CcyId = pld.CcyId,
+                Price = pld.Price,
+                Cost = pld.Cost,
+                CreatedAt = DateTime.UtcNow,
+                DiscountType = pld.DiscountType,
+                DiscountValue = pld.DiscountValue,
+                Id = 0,
+                ItemId = pld.ItemId,
+                PriceListId = pld.PriceListId,
+                PromotionId = pld.PromotionId,
+                UomId = pld.UomId
+            });
+
+            plds.AddRange(uomDefines.Select(uom => new PriceListDetailDto
+            {
+                CcyId = pld.CcyId,
+                Price = pld.Price * uom.Factor,
+                Cost = pld.Cost * uom.Factor,
+                CreatedAt = DateTime.UtcNow,
+                DiscountType = pld.DiscountType,
+                DiscountValue = pld.DiscountValue,
+                Id = 0,
+                ItemId = pld.ItemId,
+                PriceListId = pld.PriceListId,
+                PromotionId = pld.PromotionId,
+                UomId = uom.AltUomId
+            }));
+        }
+
+        await repository.CreateAsync(plds, cancellationToken);
     }
 }
