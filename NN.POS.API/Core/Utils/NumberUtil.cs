d7c1d7d438bc5c0c16771d903b70b8910a28a1df
codewithmecoder <yaty1031@gmail.com>
2024-01-13 18:53:54 +0700

    add number util

---
+++ NN.POS.API/Core/Utils/NumberUtil.cs
@@ -0,0 +1,14 @@
+﻿namespace NN.POS.API.Core.Utils;
+
+public static class NumberUtil
+{
+    public static int ToInt(this object? input)
+    {
+        return int.TryParse(input?.ToString(), out var id) ? id : 0;
+    }
+
+    public static decimal ToDecimal(this object? input)
+    {
+        return decimal.TryParse(input?.ToString(), out var id) ? id : 0;
+    }
+}
