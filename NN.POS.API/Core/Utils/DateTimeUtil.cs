c2a48125e8cb766f51aafaea25bef94e75ad33cd
codewithmecoder <yaty1031@gmail.com>
2024-01-13 18:53:30 +0700

    add datetime util

---
+++ NN.POS.API/Core/Utils/DateTimeUtil.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+
+namespace NN.POS.API.Core.Utils;
+
+public static class DateTimeUtil
+{
+    public static DateTime ToDateTime(this object? dateString)
+    {
+        return DateTime.TryParse(dateString?.ToString(), CultureInfo.InvariantCulture, out var result) 
+            ? result : new DateTime(DateTime.UtcNow.Millisecond, DateTimeKind.Utc);
+    }
+}
