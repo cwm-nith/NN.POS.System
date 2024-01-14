badc3e5bcd0db5d15a1b6374e7dd9eb28674d47e
codewithmecoder <yaty1031@gmail.com>
2024-01-14 11:51:08 +0700

    DocInv > dto

---
+++ NN.POS.Model/Dtos/DocumentInvoicings/DocumentInvoicingDto.cs
@@ -0,0 +1,14 @@
+﻿using NN.POS.Model.Enums;
+
+namespace NN.POS.Model.Dtos.DocumentInvoicings;
+
+public class DocumentInvoicingDto : IBaseDto
+{
+    public int Id { get; set; }
+    public int DocId { get; set; }
+    public string DocInvoicing { get; set; } = string.Empty;
+    public int InvoiceCount { get; set; }
+    public string PreFix { get; set; } = string.Empty;
+    public DocumentInvoicingType Type { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
