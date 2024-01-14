838d9f9b8904a519f4e46649a8e68b977549d2d0
codewithmecoder <yaty1031@gmail.com>
2024-01-14 14:06:42 +0700

    prefix > add repo

---
+++ NN.POS.API/Core/Exceptions/DocumentInvoicing/DocumentInvoicePrefixingNotFoundException.cs
@@ -0,0 +1,6 @@
+﻿namespace NN.POS.API.Core.Exceptions.DocumentInvoicing;
+
+public class DocumentInvoicePrefixingNotFoundException() : BaseException("Document Invoice Prefixing could not be found")
+{
+    public override string Code => "doc_inv_pre_nf";
+}
---
+++ NN.POS.API/Core/IRepositories/DocumentInvoicing/IDocumentInvoicePrefixingRepository.cs
@@ -0,0 +1,22 @@
+﻿using NN.POS.API.App.Queries.DocumentInvoicing;
+using NN.POS.Common.Pagination;
+using NN.POS.Model.Dtos.DocumentInvoicings;
+using NN.POS.Model.Enums;
+
+namespace NN.POS.API.Core.IRepositories.DocumentInvoicing;
+
+public interface IDocumentInvoicePrefixingRepository : IRepository
+{
+    Task<DocumentInvoicePrefixingDto> GetAsync(DocumentInvoicingType type,
+        CancellationToken cancellationToken = default);
+
+    Task<DocumentInvoicePrefixingDto> GetByIdAsync(int id,
+        CancellationToken cancellationToken = default);
+
+    Task<PagedResult<DocumentInvoicePrefixingDto>> GetPageAsync(GetDocumentInvoicePrefixingPageQuery q,
+        CancellationToken cancellationToken = default);
+
+    Task CreateAsync(DocumentInvoicePrefixingDto body, CancellationToken cancellationToken = default);
+    Task UpdateAsync(DocumentInvoicePrefixingDto body, CancellationToken cancellationToken = default);
+    Task DeleteAsync(int id, CancellationToken cancellationToken = default);
+}
