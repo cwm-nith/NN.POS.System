68e448445b2bd21d04b9a4b06749157f88e8974a
codewithmecoder <yaty1031@gmail.com>
2024-01-29 21:18:41 +0700

    pur_po > get by id

--- NN.POS.API/Controllers/V1/PurchasePOController.cs
+++ NN.POS.API/Controllers/V1/PurchasePOController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using NN.POS.API.App.Queries.Purchases;
 using NN.POS.Common.Pagination;
+using NN.POS.Model.Dtos.Purchases.PurchaseOrders;
 using NN.POS.Model.Dtos.Purchases.PurchasePO;
 
 namespace NN.POS.API.Controllers.V1;
@@ -12,5 +13,11 @@ public class PurchasePOController(IMediator mediator) : BaseApiController
     public async Task<ActionResult<PagedResult<PurchasePODto>>> GetPage([FromQuery] GetPurchasePOPageQuery query)
     {
         return Ok(await mediator.Send(query));
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<PurchaseOrderDto>> GetById(int id)
+    {
+        return Ok(await mediator.Send(new GetPurchaseOrderByIdOrInvoiceQuery(id, true)));
     }
 }
---
+++ NN.POS.API/Infra/QueryHandlers/Purchases/GetPurchasePOByIdOrInvoiceQueryHandler.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using NN.POS.API.App.Queries.Purchases;
+using NN.POS.API.Core.IRepositories.Purchases;
+using NN.POS.Model.Dtos.Purchases.PurchasePO;
+
+namespace NN.POS.API.Infra.QueryHandlers.Purchases;
+
+public class GetPurchasePOByIdOrInvoiceQueryHandler(IPurchasePORepository repository) : IRequestHandler<GetPurchasePOByIdOrInvoiceQuery, PurchasePODto>
+{
+    public async Task<PurchasePODto> Handle(GetPurchasePOByIdOrInvoiceQuery request, CancellationToken cancellationToken)
+    {
+        if (request.IsId) return await repository.GetByIdAsync((int)request.IdOrInvoice, cancellationToken);
+        return await repository.GetByInvoiceNoAsync(request.IdOrInvoice?.ToString() ?? "", cancellationToken);
+    }
+}
