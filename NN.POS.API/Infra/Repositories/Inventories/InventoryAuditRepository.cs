e3b6811045092ac54f9fe44cdd301443a8a85fe0
codewithmecoder <yaty1031@gmail.com>
2024-01-29 19:37:39 +0700

    inv_audit > add repo

---
+++ NN.POS.API/Infra/Repositories/Inventories/InventoryAuditRepository.cs
@@ -0,0 +1,24 @@
+﻿using NN.POS.API.App.Queries.Inventories;
+using NN.POS.API.Core.IRepositories.Inventories;
+using NN.POS.API.Infra.Tables.Purchases.PurchasePO;
+using NN.POS.API.Infra.Tables;
+using NN.POS.Model.Dtos.Inventories;
+using NN.POS.API.Infra.Tables.Inventories;
+
+namespace NN.POS.API.Infra.Repositories.Inventories;
+
+public class InventoryAuditRepository(
+    IReadDbRepository<InventoryAuditTable> readDbRepository,
+    IWriteDbRepository<InventoryAuditTable> writeDbRepository) : IInventoryAuditRepository
+{
+    public async Task CreateAsync(InventoryAuditDto dto, CancellationToken cancellationToken = default)
+    {
+        await writeDbRepository.AddAsync(dto.ToTable(), cancellationToken);
+    }
+
+    public async Task<List<InventoryAuditDto>> GetAsync(GetInventoryAuditQuery q, CancellationToken cancellationToken = default)
+    {
+        var data = await readDbRepository.WhereAsync(i => i.ItemId == q.ItemId && i.UomId == q.UomId && i.WarehouseId == q.WarehouseId, cancellationToken);
+        return data.Select(i => i.ToDto()).ToList();
+    }
+}
