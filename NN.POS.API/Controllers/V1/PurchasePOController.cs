22ebd22ed4099c90ee281c8087225a6ed704854d
codewithmecoder <yaty1031@gmail.com>
2024-01-29 21:14:12 +0700

    pur_po > get page

---
+++ NN.POS.API/Controllers/V1/PurchasePOController.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using NN.POS.API.App.Queries.Purchases;
+using NN.POS.Common.Pagination;
+using NN.POS.Model.Dtos.Purchases.PurchasePO;
+
+namespace NN.POS.API.Controllers.V1;
+
+public class PurchasePOController(IMediator mediator) : BaseApiController
+{
+    [HttpGet]
+    public async Task<ActionResult<PagedResult<PurchasePODto>>> GetPage([FromQuery] GetPurchasePOPageQuery query)
+    {
+        return Ok(await mediator.Send(query));
+    }
+}
---
+++ NN.POS.API/Infra/QueryHandlers/Purchases/GetPurchasePOPageQueryHandler.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using NN.POS.API.App.Queries.Purchases;
+using NN.POS.API.Core.IRepositories.Purchases;
+using NN.POS.Common.Pagination;
+using NN.POS.Model.Dtos.Purchases.PurchasePO;
+
+namespace NN.POS.API.Infra.QueryHandlers.Purchases;
+
+public class GetPurchasePOPageQueryHandler(IPurchasePORepository repository) : IRequestHandler<GetPurchasePOPageQuery, PagedResult<PurchasePODto>>
+{
+    public async Task<PagedResult<PurchasePODto>> Handle(GetPurchasePOPageQuery request, CancellationToken cancellationToken)
+    {
+        return await repository.GetPageAsync(request, cancellationToken);
+    }
+}
