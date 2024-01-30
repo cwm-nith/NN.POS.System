b109668ce3023e78ae21e54d9615c94dd5fc2397
codewithmecoder <yaty1031@gmail.com>
2024-01-30 21:07:49 +0700

    pur_po > validation

---
+++ NN.POS.Web/Validations/Purchases/PurchasePO/CreatePurchasePOValidation.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using NN.POS.Model.Dtos.Purchases.PurchasePO;
+
+namespace NN.POS.Web.Validations.Purchases.PurchasePO;
+
+public class CreatePurchasePOValidation : BaseValidator<CreatePurchasePODto>
+{
+    public CreatePurchasePOValidation()
+    {
+        RuleFor(i => i.SupplyId)
+            .GreaterThan(0)
+            .WithMessage("Please choose suppliers");
+
+        RuleFor(i => i.WarehouseId)
+            .GreaterThan(0)
+            .WithMessage("Please choose warehouse");
+
+        RuleFor(i => i.PurCcyId)
+            .GreaterThan(0)
+            .WithMessage("Please choose Purchase Currency");
+
+        RuleForEach(i => i.PurchasePODetails)
+            .SetValidator(new CreatePurchasePODetailValidation());
+    }
+}
